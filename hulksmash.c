1a745f70c8831216ff56338af4196473b3785e78
Nick Bené <bene.nick1@gmail.com>
2015-03-15 17:16:12 -0600

    prints hulksmash, user enters things, tokenizes and stores tokens

--- hulksmash.c
+++ hulksmash.c
@@ -1,24 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define INPUT_SIZE 1024
+#define ARG_SIZE 1000
 #define NAME "-hulksmash-4.20$ "
 #define QUIT "-q"
 
 int main()
 {
+	size_t ln;
+	size_t n;
+	int args;
 	char input[INPUT_SIZE];
-	
+	char * tokens[ARG_SIZE];
+
 	printf("%s ", NAME);
 	while(fgets(input, INPUT_SIZE, stdin)){
-		size_t ln = strlen(input) - 1;
+		ln = strlen(input) - 1;
 		if(input[ln] == '\n'){input[ln] = '\0';}
-
-		printf("%s\n", input);
+		
 		if(strcmp(input, QUIT) == 0){
 			break;
 		}
-
+		
+		n = 0;
+		for(char * p = strtok(input, " "); p; p = strtok(NULL, " ")){
+			if(n > ARG_SIZE){
+				break;
+			}
+			tokens[n++] = p;
+		}
+		args = n;
+		// print all args -- for testing
+		for(int i = 0 ; i < args ; ++i){
+			printf("token %d is '%s'.\n", i, tokens[i]);
+		}
+	
 		printf("%s ", NAME);
 	}
+	
+	return 0;
 }
