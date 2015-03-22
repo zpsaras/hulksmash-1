3b388f39aefecffa7c52b67f72b46da279344d31
nickbean01 <bene.nick1@gmail.com>
2015-03-22 14:32:05 -0400

    tokenizer in a function now. reduced number of lines of code

--- shell.c
+++ shell.c
@@ -7,24 +7,80 @@
 #define DELIMS " \t|\"\0"
 
 char * tokens[ARG_SIZE];
+char tok_buff[INPUT_SIZE];
+enum states {OUT, IN} state = OUT;
+int i, k, n;
+char * tok;
+
+void add_tok(){
+	tok_buff[k] = '\0';
+	tok = malloc(k);
+	memcpy(tok, tok_buff, k+1);
+	tokens[n++] = tok;
+	memset(tok_buff, '\0', k);
+	k = 0;
+
+}
+
+int tokenize(char * string, int ln){
+
+	char c, last_quote;
+
+	memset(tok_buff, '\0', INPUT_SIZE);
+	k = n = 0;
+	for(i = 0 ; i < ln ; ++i){
+		c = string[i];
+		if(state == OUT){
+			if((c == ' ') || (c == '\t') || (c == '\"') || 
+			   (c == '\'') || (c == '|')){
+				if(k > 0){
+					add_tok();
+				}
+				if((c == '\"') || (c == '\'')){
+					last_quote = c;
+					state = IN;
+				}else if(c == '|'){
+					tok_buff[k++] = '|';
+					add_tok();	
+				}
+			}else{	
+				tok_buff[k++] = c;
+				if(i == ln - 1){
+					add_tok();
+				}
+			}
+		}else if(state == IN){
+			if((c == '\"') || (c == '\'')){
+				if(c == last_quote){
+					if(k > 0){
+						add_tok();
+					}
+					state = OUT;
+				}else{
+					tok_buff[k++] = c;
+					if(i == ln - 1){
+						printf("ERROR! Must have closing quotes.\n");
+						return -1;
+					}
+				}
+			}else{
+				tok_buff[k++] = c;
+				if(i == ln - 1){
+					printf("ERROR! Must have closing quotations.\n");
+					return -1;
+				}
+			}
+		}
+	}
+
+	return n;
+}
 
 int main()
 {
-	size_t ln;
-	size_t n;
-	
-	int args;
-	int i, k;
+	int args, i, ln;
 	
 	char input[INPUT_SIZE];
-	//char * tokens[ARG_SIZE];
-	char * tok;
-
-	char c;
-	char last_quote;
-	char tok_buff[INPUT_SIZE];
-
-	enum states {OUT, IN} state = OUT;
 
 	//printf("%s ", NAME);
 	print_prompt(NAME);
@@ -37,79 +93,13 @@ int main()
 		}
 		
 		// tokenizer. ugly af gonna try to make gooder
-		k = n = 0;
-		memset(tok_buff, '\0', INPUT_SIZE);
-		for(i = 0 ; i < ln ; ++i){
-			c = input[i];
-			if(state == OUT){
-				if((c == ' ') || (c == '\t') || (c == '\"') || 
-				   (c == '\'') || (c == '|')){
-					if(k > 0){
-						tok_buff[k] = '\0';
-						tok = malloc(k);
-						memcpy(tok, tok_buff, k+1);
-						tokens[n++] = tok;
-						memset(tok_buff, '\0', k);
-						k = 0;
-					}
-					if((c == '\"') || (c == '\'')){
-						last_quote = c;
-						state = IN;
-					}else if(c == '|'){
-						tok_buff[k++] = '|';	
-						tok_buff[k] = '\0';
-						tok = malloc(k);
-						memcpy(tok, tok_buff, k+1);
-						tokens[n++] = tok;
-						memset(tok_buff, '\0', k);
-						k = 0;
-					}
-				}else{
-					tok_buff[k++] = c;
-					if(i == ln - 1){
-						tok_buff[k] = '\0';
-						tok = malloc(k);
-						memcpy(tok, tok_buff, k+1);
-						tokens[n++] = tok;
-						memset(tok_buff, '\0', k);
-						k = 0;
-					}
-				}
-			}else if(state == IN){
-				if((c == '\"') || (c == '\'')){
-					if(c == last_quote){
-						if(k > 0){
-							tok_buff[k] = '\0';
-							tok = malloc(k);
-							memcpy(tok, tok_buff, k+1);
-							tokens[n++] = tok;
-							memset(tok_buff, '\0', k);
-							k = 0;
-						}
-						state = OUT;
-					}else{
-						tok_buff[k++] = c;
-						if(i == ln - 1){
-							printf("ERROR! Must have closing quotes.\n");
-							return -1;
-						}
-					}
-				}else{
-					tok_buff[k++] = c;
-					if(i == ln - 1){
-						printf("ERROR! Must have closing quotations.\n");
-						return -1;
-					}
-				}
-			}
-		}
-		args = n;
+		args = tokenize(input, ln);
+		
 		// print all args -- for testing
-		/*
 		for(i = 0 ; i < args ; ++i){
 			printf("token %d is '%s'.\n", i, tokens[i]);
 		}
-		*/
+		
 		
 		/*
 		 * !!!TEST SECTION!!!
